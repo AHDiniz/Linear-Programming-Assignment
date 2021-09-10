50e0314f02520e28433410408807b0b987027389
AHDiniz <alanhdiniz@gmail.com>
2021-09-10 20:40:08 -0300

    Trying to create a constraint for the linear programming model

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
+from sys import argv
 from gurobipy import *
-from scipy.sparse import csr_matrix
-from scipy.sparse.csgraph import maximum_flow
 from maze import Maze, Cell, CellType
 
-maze : Maze = Maze(5, 4)
-maze.generate(0, 0)
-maze.set_different_cell_types()
-print(maze.to_adj_matrix())
+def avg_distance(variables : dict) -> int:
+    
+
+    return 0
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print("Usage: python main.py <<maze width>> <<maze height>>")
+    elif len(argv) == 2:
+        width : int = int(argv[0])
+        height : int = int(argv[1])
+    
+        maze : Maze = Maze(width, height)
+        maze.generate(0, 0)
+        maze.set_different_cell_types()
+
+        lin_prog_model : Model = Model(name = 'Game Level Generator')
+
+        # Variables that need to be set:
+        # The player position
+        # The key position
+        # The door position
+        # The enemies positions
+        # Needs to maximize distance between player, key, door and enemies
+        # Needs to stay in the limits of the graph
+        # Every position needs to be different from each other
+
+    
--- maze.py
+++ maze.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from enum import Enum
+from math import inf
 
 class CellType(Enum):
     NONE = 0
@@ -58,6 +59,7 @@ class Maze:
         self.__columns : int = columns
         self.__map : list = list([])
         self.__enemy_count : int = enemy_count
+        self.__adj_matrix : list = None
 
         for i in range(self.__rows):
             self.__map.append(list([]))
@@ -66,6 +68,15 @@ class Maze:
     
     def cell_at(self, x : int, y : int) -> Cell:
         return self.__map[x][y]
+    
+    def positions_of_type(self, cell_type : CellType) -> list:
+        result : list = []
+        for row in range(self.__rows):
+            for column in range(self.__columns):
+                cell : Cell = self.__map[row][column]
+                if cell.cell_type == cell_type:
+                    result.append((row, column))
+        return result
 
     def find_valid_neighbour(self, cell : Cell) -> list:
         delta = [('W', (-1,0)), ('E', (1,0)), ('S', (0,1)), ('N', (0,-1))]
@@ -110,7 +121,27 @@ class Maze:
             cell_stack.append(current_cell)
             current_cell = next_cell
             visited_cells += 1
-    
+        
+        self.__adj_matrix = self.to_adj_matrix()
+
+    def distance(self, a_id : int, b_id : int) -> int:
+        distances : list = [inf] * len(self.__adj_matrix)
+        last_node_in_path : list = [-1] * len(self.__adj_matrix)
+        queue : list = list(range(len(self.__adj_matrix)))
+
+        distances[a_id] = 0
+
+        while len(queue) > 0:
+            u : int = min(queue)
+            queue.remove(u)
+            for v in range(self.__adj_matrix[u]):
+                if self.__adj_matrix[u][v] == 1:
+                    if distances[v] > distances[u] + 1:
+                        distances[v] = distances[u] + 1
+                        last_node_in_path[v] = u
+        
+        return distances[b_id]
+
     def set_different_cell_types(self):
         used_cells : list = []
         enemy_type_list : list = [CellType.ENEMY_1, CellType.ENEMY_2, CellType.ENEMY_3]
