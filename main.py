9dfd145e1fa858deeded8d2bc7ef91605674a169
Alan Diniz <alanhdiniz@gmail.com>
2021-10-08 22:17:10 -0300

    Well, looks like the model is too big for the gurobi license...

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ from sys import argv
 from gurobipy import *
 from maze import Maze, Cell, CellType
 
+def capacity_constraint(capacity : dict, flow : tupledict, edge : tuple) -> TempConstr:
+    return flow[edge] <= capacity[edge]
+
+def key_flow_constraint(flow : tupledict, maze : Maze) -> TempConstr:
+    key_indexes = maze.key_mat_indexes
+    return flow[key_indexes] == 1
+
+def objective(cost : dict, flow : tupledict, edges : list) -> LinExpr:
+    print(cost[edges[0]])
+    result : LinExpr = cost[edges[0]] * flow[edges[0]]
+    for i in range(1, len(edges)):
+        result += cost[edges[i]] * flow[edges[i]]
+    return result
+
 if len(argv) < 3:
     print("Usage: python main.py <<maze width>> <<maze height>>")
 else:
@@ -27,23 +41,23 @@ else:
     capacities : dict = {}
     costs : dict = {}
     edges : list = []
+
+    enemy_positions : list = maze.enemies_mat_indexes
+
     for row in range(adj_matrix.shape[0]):
         for column in range(adj_matrix.shape[1]):
             edge : tuple = (row, column)
             edges.append(edge)
             capacities[edge] = adj_matrix[row][column]
-            costs[edge] = 1 if adj_matrix[row][column] == 1 else np.inf
+            costs[edge] = 1 if (row, column) in enemy_positions else 99
+
+    flow = model.addVars(edges, name = "flow")
+
+    model.addConstr(key_flow_constraint(flow, maze))
+
+    for edge in edges:
+        model.addConstr(capacity_constraint(capacities, flow, edge))
     
-    capacity = multidict(capacities)
-    cost = multidict(costs)
-
-    flow = model.addVars(edges, obj = cost[1], name = "flow")
-
-    capacity_constraint = model.addConstrs(flow.select(edges[i][0], edges[i][1]) <= capacity[i] for i in range(len(edges)))
-
-    key_indexes = maze.key_mat_indexes
-    key_flow_constraint = model.addConstrs(flow.select(key_indexes[0], key_indexes[1]) == 1)
-
-    model.setObjective(quicksum(flow.select(edges[i][0], edges[i][1]) * cost[i] for i in range(len(edges))), GRB.MINIMIZE)
+    model.setObjective(objective(costs, flow, edges), GRB.MINIMIZE)
 
     model.optimize()
